3e1f37810fbdb1ef7e432bd2e4528dac1a6f6059
Roland W-H <45169084+RolandWH@users.noreply.github.com>
2020-06-13 11:29:56 +0100

    added ore generation

--- src/main/java/com/shadow/sapphiremc/SapphireMC.java
+++ src/main/java/com/shadow/sapphiremc/SapphireMC.java
@@ -1,28 +1,28 @@
 package com.shadow.sapphiremc;
 
 import com.shadow.sapphiremc.init.ItemInit;
-import net.minecraft.block.Block;
+import com.shadow.sapphiremc.world.gen.ModOreGen;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DeferredWorkQueue;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.lifecycle.*;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("sapphiremc")
+@Mod.EventBusSubscriber(modid = SapphireMC.MOD_ID, bus = Bus.MOD)
 public class SapphireMC
 {
     // Directly reference a log4j logger.
@@ -51,11 +51,13 @@ public class SapphireMC
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
         LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+        DeferredWorkQueue.runLater(ModOreGen::generateOre);
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event)
@@ -77,6 +79,7 @@ public class SapphireMC
         // do something when the server starts
         LOGGER.info("HELLO from server starting");
     }
+
 
     public static class SapphireItemGroup extends ItemGroup {
         public static final SapphireItemGroup instance = new SapphireItemGroup(ItemGroup.GROUPS.length, "sapphiretab");
--- src/main/java/com/shadow/sapphiremc/world/gen/ModOreGen.java
+++ src/main/java/com/shadow/sapphiremc/world/gen/ModOreGen.java
@@ -1,4 +1,20 @@
 package com.shadow.sapphiremc.world.gen;
 
+import com.shadow.sapphiremc.init.BlockInit;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.placement.ConfiguredPlacement;
+import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ModOreGen {
+    public static void generateOre() {
+        for (Biome biome : ForgeRegistries.BIOMES) {
+            ConfiguredPlacement modGenConfig = Placement.COUNT_RANGE.configure(new CountRangeConfig(4, 3, 4, 14));
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockInit.sapphire_ore.getDefaultState(), 8)).withPlacement(modGenConfig));
+        }
+    }
 }
