12a4abc1dbdaa7827b1f18e0a5069a77bfe9ce5a
Roland W-H <45169084+RolandWH@users.noreply.github.com>
2020-06-24 19:51:40 +0100

    tweaked sapphire vault

--- src/main/java/com/shadow/sapphiremc/init/BlockInit.java
+++ src/main/java/com/shadow/sapphiremc/init/BlockInit.java
@@ -15,7 +15,7 @@ public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, SapphireMC.MOD_ID);
 
     // Blocks
-    public static final RegistryObject<Block> SAPPHIRE_BLOCK = BLOCKS.register("sapphire_block", () -> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(5.0f, 30f).sound(SoundType.METAL).harvestLevel(3).harvestTool(ToolType.PICKAXE)));
-    public static final RegistryObject<Block> SAPPHIRE_ORE = BLOCKS.register("sapphire_ore", () -> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(3.0f, 15f).sound(SoundType.METAL).harvestLevel(3).harvestTool(ToolType.PICKAXE)));
-    public static final RegistryObject<Block> SAPPHIRE_VAULT = BLOCKS.register("sapphire_vault", () -> new SapphireVault(Block.Properties.create(Material.IRON)));
+    public static final RegistryObject<Block> SAPPHIRE_BLOCK = BLOCKS.register("sapphire_block", () -> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(5.0f, 30.0f).sound(SoundType.METAL).harvestLevel(3).harvestTool(ToolType.PICKAXE)));
+    public static final RegistryObject<Block> SAPPHIRE_ORE = BLOCKS.register("sapphire_ore", () -> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(3.0f, 15.0f).sound(SoundType.METAL).harvestLevel(3).harvestTool(ToolType.PICKAXE)));
+    public static final RegistryObject<Block> SAPPHIRE_VAULT = BLOCKS.register("sapphire_vault", () -> new SapphireVault(Block.Properties.create(Material.IRON).hardnessAndResistance(5.0f, 12.5f).sound(SoundType.METAL).harvestLevel(1).harvestTool(ToolType.PICKAXE)));
 }
